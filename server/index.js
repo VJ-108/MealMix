244ee7de581693cad53b09f4331b294d3b0f3695
VJ-108 <vaibhav160205@gmail.com>
2024-08-09 10:40:53 +0530

    updated files for deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { configDotenv } from "dotenv";
+import path from "path";
 configDotenv();
 
 const app = express();
@@ -25,9 +26,18 @@ import recipeRouter from "./routes/recipe.routes.js";
 app.use("/api/user", userRouter);
 app.use("/api/recipe", recipeRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
